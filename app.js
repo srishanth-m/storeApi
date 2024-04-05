e195eeab45877c70465eb029ed32a67a96a9b091
srihanth-m <srishanthmule@gmail.com>
2024-04-05 14:11:43 +0530

    connected db to the backend

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middleware/not-found.js')
+const errorMiddleware = require('./middleware/error-handler.js')
+
+const connectDB = require('./db/connect.js')
+const productsRouter = require('./routes/products.js')
+
+app.use(express.json())
+
+app.get('/' , (req , res) =>{
+    res.send('<h1>Store Api</h1><a href="/api/v1/products"> products route</a>')
+})
+
+app.use('/api/v1/products' , productsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 5000
+
+const start = async() =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port , console.log(`app listening at port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
---
+++ controllers/products.js
@@ -0,0 +1,12 @@
+const getAllProductsStatic = (req, res) => {
+  res.status(200).json({ msg: "products testing route" });
+};
+
+const getAllProducts = (req, res) => {
+  res.status(200).json({ msg: "products route" });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
